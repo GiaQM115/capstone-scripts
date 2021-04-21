15e0bd353673b3dbebbe749508794781458263c0
Us <nah@bro.com>
2021-04-20 21:37:11 -0400

    add support for user manager to setup script

--- real_code/setup_misp.sh
+++ real_code/setup_misp.sh
@@ -17,7 +17,7 @@ docker stop MISP
 docker rm MISP
 docker stop comparison-server
 docker rm comparison-server
-
+<<COMMENT
 echo "Fetching MISP instance"
 git clone https://github.com/harvard-itsecurity/docker-misp.git
 
@@ -26,10 +26,10 @@ read mysql
 
 echo -n "Set your GPG password: "
 read gpg
-
+COMMENT
 echo -n "Set the FQDN for your MISP instance: "
 read fqdn
-
+<<COMMENT
 echo -n "Set your postfix relay, or press Enter for default (localhost): "
 read postfix
 
@@ -63,7 +63,7 @@ docker run --name=MISP -d -p 443:443 -p 80:80 -p 3306:3306 -p 6666:6666 -v /etc/
 echo "Server will be up momentarily. Please go to https://$fqdn and login with '$email', PW: 'admin'"
 
 cd ../
-
+COMMENT
 echo -n "Server Listening Port: "
 read port
 
@@ -80,24 +80,45 @@ dir=`pwd`
 
 cp $dir/client_sock_backup client_sock.py
 cp $dir/correlate_backup correlate.py
+cp $dir/fetch_backup fetch.py
 
 echo "Preparing scripts"
 
 sed -i "s/MISP_SERVER_PORT/$port/" client_sock.py
+
 sed -i "s/MISP_ORG/$mid/" correlate.py
 sed -i "s/ALERT_THRESHOLD/$threshold/" correlate.py
 sed -i "s/MISP_FQDN/$fqdn/" correlate.py
 sed -i "s/MISP_AUTH_KEY/$key/" correlate.py
 
+sed -i "s/MISP_FQDN/$fqdn/" fetch.py
+sed -i "s/MISP_AUTH_KEY/$key" fetch.py
+
 echo -n "Scheduling watchdog"
 
 crontab -l > cronlist > /dev/null
 echo "0 * * * * bash $dir/watchdog.sh" >> cronlist
+echo -n "Scheduling daily feed fetch"
+echo "0 0 * * * bash $dir/fetch.py" >> cronlist
 crontab cronlist > /dev/null
 rm cronlist
 
-echo -n "Building Docker image"
+echo "Building Docker image"
 docker build -t correlation-base .
 echo -n "Running container"
 docker run --name=comparison-server -d -p $port:$port correlation-base
+
+mkdir -p user_manager/mysql
+
+echo -n "User Manager MySQL admin password: "
+read umpass
+
+echo "Starting user manager"
+docker build -t usermgt -f UMDockerfile
+docker run --name=user-manager -d \
+	-p 3000:3306 -p 8080:80 \
+	-v user_manager/mysql:/var/lib/mysql \
+	-e MYSQL_ADMIN_PASS="$umpass" \
+	usermgt
+
 docker ps
